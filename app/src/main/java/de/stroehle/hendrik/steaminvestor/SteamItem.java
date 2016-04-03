66b3404a2ca1f82dd0ccea699615257b98c03c6e
Matze <xatorax@googlemail.com>
2016-04-03 21:42:41 +0200

    Added methods to SteamItem

--- app/src/main/java/de/stroehle/hendrik/steaminvestor/SteamItem.java
+++ app/src/main/java/de/stroehle/hendrik/steaminvestor/SteamItem.java
@@ -1,18 +1,54 @@
 package de.stroehle.hendrik.steaminvestor;
 
+import java.io.IOException;
 import java.net.URLDecoder;
+
 
 import java.io.UnsupportedEncodingException;
 
+
+import de.stroehle.hendrik.steaminvestor.DataGrabber;
+
 public class SteamItem {
-    public String ItemName;
+    private String ItemName;
     private String ItemNameReadable;
+    private Double CurrentPriceCached = 0.0;
 
 
     public SteamItem(String name){
         this.ItemName = name;
     }
 
+
+
+    public double getCurrentPriceCached(){
+        double price;
+        if (this.CurrentPriceCached.equals(0.0)){
+            price = this.getCurrentPrice();
+        }
+        else{
+            price = this.CurrentPriceCached;
+        }
+
+        return price;
+    }
+
+    public double getCurrentPrice(){
+        double price;
+        try{
+            price = DataGrabber.GetCurrentPriceFromItemName(this.ItemName);
+            this.CurrentPriceCached = price;
+        }
+        catch (IOException e){
+            price = 999;
+        }
+        return price;
+    }
+
+
+    public String getItemName(){
+        return this.ItemName;
+    }
 
 
     public String getNameReadable(){
